0eae552297cfe73ac3213f0d3240a811b0036153
Julien-Martin-g404 <julienmartin2802@gmail.com>
2023-02-17 11:38:02 +0100

    initialisation portfolio

---
+++ script.js
@@ -0,0 +1,20 @@
+const navLinks = document.querySelectorAll('nav a');
+
+navLinks.forEach(link => {
+    link.addEventListener('click', function (event) {
+        event.preventDefault(); // Empêcher le comportement par défaut du lien
+
+        // Récupérer l'élément cible à partir de l'attribut "href" du lien
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+
+        // Ajouter la classe "active" au lien cliqué et supprimer la classe "active" de tous les autres liens
+        navLinks.forEach(link => link.classList.remove('active'));
+        this.classList.add('active');
+
+        // Faire défiler la page vers l'élément cible
+        targetElement.scrollIntoView({ behavior: "smooth" });
+    });
+});
+
+
