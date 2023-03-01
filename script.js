3d7595fbbf5755019020c86dc16ec445728bc741
Julien-Martin-g404 <julienmartin2802@gmail.com>
2023-03-01 10:47:40 +0100

    dépôt fichier portfolio julien 1-03-23

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 const navLinks = document.querySelectorAll('nav a');
 
 window.addEventListener('scroll', () => {
-  const {scrollTop, clientHeight} = document.documentElement;
+    const { scrollTop, clientHeight } = document.documentElement;
 
-  console.log(scrollTop, clientHeight);
+    console.log(scrollTop, clientHeight);
 });
+
 
 navLinks.forEach(link => {
     link.addEventListener('click', function (event) {
-        event.preventDefault(); // Empêcher le comportement par défaut du lien
+        // event.preventDefault(); // Empêcher le comportement par défaut du lien
 
         // Récupérer l'élément cible à partir de l'attribut "href" du lien
         const targetId = this.getAttribute('href');
@@ -24,4 +25,49 @@ navLinks.forEach(link => {
 });
 
 
+let barre = document.querySelector('#Skill-HTML');
+let barre2 = document.querySelector('#Skill-CSS');
+let barre3 = document.querySelector('#Skill-BOOTSTRAP');
+let barre4 = document.querySelector('#Skill-PHP');
+let barre5 = document.querySelector('#Skill-SQL');
+let barre6 = document.querySelector('#Skill-JS');
 
+window.addEventListener('scroll', () => {
+    const { scrollTop } = document.documentElement;
+
+    if (scrollTop >= 650 && scrollTop <= 800) {
+
+        barre.classList.add('Skill-HTML');
+        barre2.classList.add('Skill-CSS');
+        barre3.classList.add('Skill-BOOTSTRAP');
+        barre4.classList.add('Skill-PHP');
+        barre5.classList.add('Skill-SQL');
+        barre6.classList.add('Skill-JS');
+
+
+        console.log('Animation déclenchée!');
+    }
+});
+
+
+// Récupération des éléments HTML
+const nav = document.getElementById('sidebar');
+const burgerMenu = document.getElementById('burger-menu');
+
+// Ajout d'un événement au clic sur le burger menu
+burgerMenu.addEventListener('click', function () {
+    // Affichage de la nav
+    nav.style.display = 'block';
+});
+
+// Ajout d'un événement au clic sur un lien de la nav
+nav.addEventListener('click', function (event) {
+    // Vérification que le clic a eu lieu sur un lien de la nav
+    if (event.target.tagName === 'A') {
+        // Vérification de la largeur de l'écran
+        if (window.innerWidth <= 800) {
+            // Disparition de la nav
+            nav.style.display = 'none';
+        }
+    }
+});
